75ee4153238692ef9379c7a2f59e013508452e05
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-03-09 10:56:37 +0330

    add max length for messages history

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -1,18 +1,21 @@
+from collections import deque
+
 import openai
 from config import API_KEY, AI_MODEL
 
 
 class ChatGPT:
+    MAX_MESSAGES = 10
+
     def __init__(self):
         openai.api_key = API_KEY
-        self.messages = []
+        self.messages = deque(maxlen=self.MAX_MESSAGES)
 
     def send_msg(self, message: str):
-        user_input = message
-        self.messages.append({'role': 'user', 'content': user_input})
+        self.messages.append({'role': 'user', 'content': message})
         completion = openai.ChatCompletion.create(
             model=AI_MODEL,
-            messages=self.messages
+            messages=list(self.messages)
         )
         ai_response = completion.choices[0].message['content']
         self.messages.append({'role': 'assistant', 'content': ai_response})
